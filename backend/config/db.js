7704e3dbcbea0ef4bd2cc038bf064d31a3fb0fb4
CODEWITHSOURAVBANERJEE26 <147583018+CODEWITHSOURAVBANERJEE26@users.noreply.github.com>
2026-07-10 19:16:57 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,3 @@
-
-/*
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(error);   // <-- This is the only important change
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
-
-*/
-
-
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
