a00fb68f175ff52e3c83bf8f8935e450d052d135
CODEWITHSOURAVBANERJEE26 <147583018+CODEWITHSOURAVBANERJEE26@users.noreply.github.com>
2026-07-12 12:43:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,72 +1,81 @@
 import { useState } from "react";
+import "./App.css";
 import NotificationBell from "./components/NotificationBell";
 
 function App() {
+
     const [tenantId, setTenantId] = useState("t1");
     const [userId, setUserId] = useState("u1");
 
     return (
-        <div
-            style={{
-                width: "750px",
-                margin: "30px auto",
-                fontFamily: "Arial"
-            }}
-        >
-            <h1 style={{ textAlign: "center" }}>
+
+        <div className="container">
+
+            <h1 className="title">
                 Multi-Tenant Notification System
             </h1>
 
-            <div
-                style={{
-                    display: "flex",
-                    gap: "15px",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    marginBottom: "20px"
-                }}
-            >
-                <div>
+            <div className="selector-box">
+
+                <div className="selector">
+
                     <label>Tenant</label>
-                    <br />
+
                     <select
                         value={tenantId}
                         onChange={(e) => {
+
                             const tenant = e.target.value;
+
                             setTenantId(tenant);
 
                             if (tenant === "t1") {
+
                                 setUserId("u1");
+
                             } else {
+
                                 setUserId("u2");
+
                             }
+
                         }}
                     >
+
                         <option value="t1">Tenant 1</option>
                         <option value="t2">Tenant 2</option>
+
                     </select>
+
                 </div>
 
-                <div>
+                <div className="selector">
+
                     <label>User</label>
-                    <br />
+
                     <select
                         value={userId}
                         onChange={(e) => setUserId(e.target.value)}
                     >
+
                         <option value="u1">User 1</option>
                         <option value="u2">User 2</option>
+
                     </select>
+
                 </div>
+
             </div>
 
             <NotificationBell
                 tenantId={tenantId}
                 userId={userId}
             />
+
         </div>
+
     );
+
 }
 
 export default App;
-
